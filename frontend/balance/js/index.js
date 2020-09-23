10786aedccf7ea3ba926931c7243dda742967c81
ali salemi <alisalemmi@outlook.com>
2020-09-23 15:10:14 +0330

    دو سه خط کد اولیه

---
+++ frontend/balance/js/index.js
@@ -0,0 +1,27 @@
+import '../scss/main.scss';
+
+// const DOM = {
+//   polygons: document.querySelectorAll('.polygon')
+// };
+
+// DOM.polygons.forEach(p => {
+//   p.style.left = '10rem';
+// });
+
+// /**
+//  * generate a regular polygon. return points string.
+//  * @param {Number} v number of vertices
+//  */
+// const polygonGenerator = v => {
+//   const r = 100;
+//   let points = '';
+
+//   for (let i = 0; i < v; i++) {
+//     const d = (i / v) * 2 * Math.PI;
+//     points += `${r * Math.cos(d) + r},${r * Math.sin(d) + r} `;
+//   }
+
+//   return points;
+// };
+
+// const polygon = document.getElementById('polygon').getContext('2d');
