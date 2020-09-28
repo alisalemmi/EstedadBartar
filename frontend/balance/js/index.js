9436f4b034463cbae00e6dc5ede12dee9fb4fa97
ali salemi <alisalemmi@outlook.com>
2020-09-28 19:26:09 +0330

    بهبود حرکت توپ و امتیاز و حالت برخورد آن ها تابع های سرعت و زمان تغییر سرعت نیاز به اصلاح دارند

--- frontend/balance/js/index.js
+++ frontend/balance/js/index.js
@@ -9,13 +9,30 @@ import * as Touch from './view/touch';
 import * as Ball from './view/ball';
 import * as Score from './view/score';
 
+let rfa = 0;
+let startTime = -1;
+let previousTime = -1;
+
 const finish = () => {
+  cancelAnimationFrame(rfa);
+  startTime = -1;
+
   Buble.stop();
   Ball.stop();
-  // Touch.stop();
   Score.stop();
 
   document.querySelector('#check__score').checked = true;
+};
+
+const animate = time => {
+  if (startTime === -1) {
+    startTime = time;
+    previousTime = time;
+  }
+
+  Ball.animate(time - startTime, time - previousTime);
+  previousTime = time;
+  rfa = requestAnimationFrame(animate);
 };
 
 const start = () => {
@@ -26,8 +43,10 @@ const start = () => {
   // 2. motion
   Motion.addHandler(e => {
     Buble.moveBuble(e); // TODO handle reject
-    Buble.validate(finish);
+    // Buble.validate(finish);
   });
+
+  rfa = requestAnimationFrame(animate);
 };
 
 Play.playButtonHandler(() => {
@@ -36,30 +55,4 @@ Play.playButtonHandler(() => {
 
 Touch.start(start);
 
-Touch.validate(finish);
-
-// const DOM = {
-//   polygons: document.querySelectorAll('.polygon')
-// };
-
-// DOM.polygons.forEach(p => {
-//   p.style.left = '10rem';
-// });
-
-// /**
-//  * generate a regular polygon. return points string.
-//  * @param {Number} v number of vertices
-//  */
-// const polygonGenerator = v => {
-//   const r = 100;
-//   let points = '';
-
-//   for (let i = 0; i < v; i++) {
-//     const d = (i / v) * 2 * Math.PI;
-//     points += `${r * Math.cos(d) + r},${r * Math.sin(d) + r} `;
-//   }
-
-//   return points;
-// };
-
-// const polygon = document.getElementById('polygon').getContext('2d');
+// Touch.validate(finish);
--- frontend/balance/js/view/ball.js
+++ frontend/balance/js/view/ball.js
@@ -1,55 +1,104 @@
 const DOM = {
   puzzle: document.querySelector('.puzzle'),
-  ball: document.querySelector('.ball')
+  ball: document.querySelector('.ball'),
+  score: document.querySelector('.score')
 };
 
-let raf = 0;
-
-const v = [0, 0];
-const p = [0, 0];
-let r;
+const v = [0, 0, 0, 0];
+const p = [0, 0, 0, 0];
+const r = [80, 48];
 let w;
 let h;
 
-const clear = () => {
-  cancelAnimationFrame(raf);
+const minSpeed = 0.01;
+let nextStepBall = 1000;
+// let nextStepScore = 500;
 
-  v[0] = Math.random() * 2 - 1 || 0.5;
-  v[1] = Math.random() * 2 - 1 || -0.5;
-
-  r = DOM.ball.offsetWidth / 2;
-  w = DOM.puzzle.offsetWidth / 2;
-  h = DOM.puzzle.offsetHeight / 2;
+const getRandomSpeed = time => {
+  const domain = 0.2 * Math.log10(time / 8 + 1) + 0.1;
+  return Math.random() * domain - domain / 2 || minSpeed;
 };
 
-const updateInfo = () => {
-  // prevent goiong out
-  if (p[0] + v[0] + r > w || p[0] + v[0] - r < -w) v[0] *= -0.9;
-  if (p[1] + v[1] + r > h || p[1] + v[1] - r < -h) v[1] *= -0.9;
+const getNextStep = time => {
+  const min = 2300 - 400 * Math.log10(time / 100);
+  const domain = 40 * Math.sqrt(time / 80);
 
-  // prevent accident
+  return Math.random() * domain + min;
+};
 
-  // update position
-  p[0] += v[0];
-  p[1] += v[1];
+const clear = () => {
+  r[0] = DOM.ball.offsetWidth / 2;
+  r[1] = DOM.score.offsetWidth / 2;
+  w = DOM.puzzle.offsetWidth / 2;
+  h = DOM.puzzle.offsetHeight / 2;
 
-  // update speed
+  p[0] = (Math.random() * 2 - 1) * (w - r[0]);
+  p[1] = (Math.random() * 2 - 1) * (h - r[0]);
+  p[2] = 0;
+  p[3] = r[1] - h;
+
+  v[0] = getRandomSpeed(0);
+  v[1] = getRandomSpeed(0);
+  v[2] = getRandomSpeed(0);
+  v[3] = getRandomSpeed(0);
+};
+
+const preventGoOut = (n, elapsedTime) => {
+  const bound = n % 2 ? h : w;
+  const rt = n / 2 >= 1 ? r[1] : r[0];
+
+  if (p[n] + v[n] * elapsedTime + rt >= bound)
+    v[n] = -Math.max(Math.abs(v[n]) * 0.99, minSpeed);
+  else if (p[n] + v[n] * elapsedTime - rt <= -bound)
+    v[n] = Math.max(Math.abs(v[n]) * 0.99, minSpeed);
+};
+
+const updateInfo = (time, elapsedTime) => {
+  // console.log('e:', elapsedTime, '\tt:', time);
+  // 1. prevent go out
+  preventGoOut(0, elapsedTime); // ball - X axis
+  preventGoOut(1, elapsedTime); // ball - Y axis
+
+  preventGoOut(2, elapsedTime); // score - X axis
+  preventGoOut(3, elapsedTime); // score - Y axis
+
+  // 2. prevent accident
+  const m = [r[0] ** 2, r[1] ** 2];
+
+  if (
+    (p[0] + v[0] * elapsedTime - (p[2] + v[2] * elapsedTime)) ** 2 +
+      (p[1] + v[1] * elapsedTime - (p[3] + v[3] * elapsedTime)) ** 2 <=
+    (r[0] + r[1]) ** 2
+  ) {
+    v[0] = ((m[0] - m[1]) * v[0] + 2 * m[1] * v[2]) / (m[0] + m[1]);
+    v[1] = ((m[0] - m[1]) * v[1] + 2 * m[1] * v[3]) / (m[0] + m[1]);
+
+    v[2] = ((m[1] - m[0]) * v[2] + 2 * m[0] * v[0]) / (m[0] + m[1]);
+    v[3] = ((m[1] - m[0]) * v[3] + 2 * m[0] * v[1]) / (m[0] + m[1]);
+  }
+
+  // 3. update position
+  p[0] += v[0] * elapsedTime;
+  p[1] += v[1] * elapsedTime;
+  p[2] += v[2] * elapsedTime;
+  p[3] += v[3] * elapsedTime;
+
+  // 4. update speed
 };
 
 const draw = () => {
-  DOM.ball.style.transform = `translate(${p[0] + v[0]}px, ${p[1] + v[1]}px)`;
+  DOM.ball.style.transform = `translate(${p[0]}px, ${p[1]}px)`;
+  DOM.score.style.transform = `translate(${p[2]}px, ${p[3]}px)`;
 };
 
-const call = () => {
+export const animate = (totalTime, elapsedTime) => {
+  updateInfo(totalTime, elapsedTime);
   draw();
-  updateInfo();
-  raf = requestAnimationFrame(call);
 };
+
 export const start = () => {
   clear();
-  raf = requestAnimationFrame(call);
+  draw();
 };
 
-export const stop = () => {
-  cancelAnimationFrame(raf);
-};
+export const stop = () => {};
